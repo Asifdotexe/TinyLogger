05eda4d05263cc6694c93dcaf1a673c1cdcd755d
Asif Sayyed <asifdotexe@gmail.com>
2025-11-02 14:44:25 +0530

    #14 updated the version number

--- src/littlelogger/__init__.py
+++ src/littlelogger/__init__.py
@@ -3,6 +3,6 @@ TinyLogger: A lightweight, zero-setup decorator for logging ML experiments.
 """
 from .decorator import log_run
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ["log_run"]
