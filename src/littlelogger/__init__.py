c05fc696a15bbea8a9806511da4221135d517e01
Asif Sayyed <asifdotexe@gmail.com>
2025-11-08 14:39:55 +0530

    #18 simplified the log loading

--- src/littlelogger/__init__.py
+++ src/littlelogger/__init__.py
@@ -2,7 +2,8 @@
 TinyLogger: A lightweight, zero-setup decorator for logging ML experiments.
 """
 from .decorator import log_run
+from .util import load_log
 
 __version__ = "1.1.0"
 
-__all__ = ["log_run"]
+__all__ = ["log_run", "load_log"]
